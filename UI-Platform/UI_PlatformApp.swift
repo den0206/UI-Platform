40fd24450c600e6ebf2134961f4a1871ccb6bfcf
den0206 <mac.pop@mineo.jp>
2020-11-24 20:52:02 +0900

    Initial Commit

---
+++ UI-Platform/UI_PlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_PlatformApp.swift
+//  UI-Platform
+//
+//  Created by 酒井ゆうき on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UI_PlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
